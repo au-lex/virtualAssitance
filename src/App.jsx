133e02063766dabebed62b12604fde347a12d2aa
au-lex <aulexc4d2@gmail.com>
2023-10-02 20:37:13 +0100

    oop

--- src/App.jsx
+++ src/App.jsx
@@ -52,34 +52,34 @@ function App() {
 
             <button
                 style={{ marginTop: "100px", marginLeft: "500px" }}
-                onMouseEnter={(e) => handleMouseEnter('Hover over Button 3', 'path_to_hover_gif3.gif', e)}
+                onMouseEnter={(e) => handleMouseEnter('Hover over Button 3',  "https://res.cloudinary.com/durbee4ln/image/upload/v1696268727/giphy_khrzov.gif", e)}
                 onMouseLeave={handleMouseLeave}
-                onClick={() => handleClick('Button 3 does DEF', 'path_to_gif3.gif')}
+                onClick={() => handleClick('Button 3 does DEF',  "https://res.cloudinary.com/durbee4ln/image/upload/v1696268727/giphy_khrzov.gif")}
             >
                 Button 3
             </button>
 
             <button
                 style={{ marginTop: "250px", marginLeft: "100px" }}
-                onMouseEnter={(e) => handleMouseEnter('Hover over Button 4', 'path_to_hover_gif4.gif', e)}
+                onMouseEnter={(e) => handleMouseEnter('Hover over Button 4',  "https://res.cloudinary.com/durbee4ln/image/upload/v1696268727/giphy_khrzov.gif", e)}
                 onMouseLeave={handleMouseLeave}
-                onClick={() => handleClick('Button 4 does GHI', 'path_to_gif4.gif')}
+                onClick={() => handleClick('Button 4 does GHI',  "https://res.cloudinary.com/durbee4ln/image/upload/v1691832901/16_prev_ui_tojjsk.png")}
             >
                 Button 4
             </button>
 
             <button
                 style={{ marginTop: "350px", marginLeft: "400px" }}
-                onMouseEnter={(e) => handleMouseEnter('Hover over Button 5', 'path_to_hover_gif5.gif', e)}
+                onMouseEnter={(e) => handleMouseEnter('Hover over Button 5', "https://res.cloudinary.com/durbee4ln/image/upload/v1696268727/giphy_khrzov.gif", e)}
                 onMouseLeave={handleMouseLeave}
-                onClick={() => handleClick('Button 5 does JKL', 'path_to_gif5.gif')}
+                onClick={() => handleClick('Button 5 does JKL', "https://res.cloudinary.com/durbee4ln/image/upload/v1691832901/16_prev_ui_tojjsk.png")}
             >
                 Button 5
             </button>
 
-            <div className={`assistant ${content.message ? 'show' : ''}`} style={{ left: `${position.left}px`, top: `${position.top}px` }}>
+            <div className={`assistant w-[100px] h-[100px] ${content.message ? 'show' : ''}`} style={{ left: `${position.left}px`, top: `${position.top}px` }}>
                 <p>{content.message}</p>
-                <img src={content.gifPath} alt="GIF for illustration" />
+                <img src={content.gifPath} alt="GIF for illustration" className="w-[100%]" />
             </div>
         </div>
     );
